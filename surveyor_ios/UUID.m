c061a1ee3bc4715cf14f3d4bc4f70563bfb4f6c4
Mark Yoon <yoon@northwestern.edu>
2011-07-25 14:11:54 -0500

    uuids. closes #21

---
+++ surveyor_ios/UUID.m
@@ -0,0 +1,16 @@
+//
+//  UUID.m
+//  surveyor_ios
+//
+//  Created by Mark Yoon on 7/25/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UUID.h"
+
+
+@implementation UUID
+
++ (NSString *)generateUuidString;
+
+@end
