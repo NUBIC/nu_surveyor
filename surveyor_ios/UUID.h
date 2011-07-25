c0a827dfa7eb7cf9a77b5dcfc69d0b32b8d1c712
Mark Yoon <yoon@northwestern.edu>
2011-07-25 15:00:03 -0500

    fixing UUIDs

--- surveyor_ios/UUID.h
+++ surveyor_ios/UUID.h
@@ -13,25 +13,6 @@
     
 }
 
-// return a new autoreleased UUID string
-// http://blog.ablepear.com/2010/09/creating-guid-or-uuid-in-objective-c.html
-+ (NSString *)generateUuidString
-{
-  // create a new UUID which you own
-  CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
-  
-  // create a new CFStringRef (toll-free bridged to NSString)
-  // that you own
-  NSString *uuidString = (NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
-  
-  // transfer ownership of the string
-  // to the autorelease pool
-  [uuidString autorelease];
-  
-  // release the UUID
-  CFRelease(uuid);
-  
-  return uuidString;
-}
++ (NSString *)generateUuidString;
 
 @end
--- surveyor_ios/UUID.m
+++ surveyor_ios/UUID.m
@@ -11,6 +11,25 @@
 
 @implementation UUID
 
-+ (NSString *)generateUuidString;
+
+// return a new autoreleased UUID string
+// http://blog.ablepear.com/2010/09/creating-guid-or-uuid-in-objective-c.html
++ (NSString *)generateUuidString{
+  // create a new UUID which you own
+  CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+  
+  // create a new CFStringRef (toll-free bridged to NSString)
+  // that you own
+  NSString *uuidString = (NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+  
+  // transfer ownership of the string
+  // to the autorelease pool
+  [uuidString autorelease];
+  
+  // release the UUID
+  CFRelease(uuid);
+  
+  return uuidString;
+}
 
 @end
