4c45efa69b48a6372a06e5be4bd00b8cb578a013
John Dzak <john.dzak@gmail.com>
2012-02-23 22:36:17 -0600

    Encapsulate constants in header file so it's easier to include surveyor in projects.

---
+++ NUSurveyor/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  NUSurveyor
+//
+//  Created by John Dzak on 2/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef NUSurveyor_Constants_h
+#define NUSurveyor_Constants_h
+
+#define RGB(r, g, b)		[UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
+#define RGBA(r, g, b, a)	[UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#define NS_YES [NSNumber numberWithBool:YES]
+#define NS_NO  [NSNumber numberWithBool:NO]
+
+
+#endif
