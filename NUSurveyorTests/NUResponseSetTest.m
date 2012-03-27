77aabddd5b5193717b1aec9fcc883aa728d931b5
John Dzak <john.dzak@gmail.com>
2012-03-27 14:38:04 -0500

    Switch response set test to use database superclass.

--- NUSurveyorTests/NUResponseSetTest.m
+++ NUSurveyorTests/NUResponseSetTest.m
@@ -12,27 +12,11 @@
 
 @implementation NUResponseSetTest
 
-@synthesize bundle = _bundle, coord = _coord, ctx = _ctx, model = _model, store = _store;
-
 NUResponseSet* rs;
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
 	
-    // Set-up code here.
-	self.bundle = [NSBundle bundleWithIdentifier:@"NUBIC.NUSurveyorTests"];
-	self.model = [[NSManagedObjectModel alloc] initWithContentsOfURL:[self.bundle URLForResource:@"NUSurveyor" withExtension:@"mom"]];
-	self.coord = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: self.model];
-	self.store = [self.coord addPersistentStoreWithType: NSInMemoryStoreType
-                                          configuration: nil
-                                                    URL: nil
-                                                options: nil 
-                                                  error: NULL];
-	self.ctx = [[NSManagedObjectContext alloc] init];
-	[self.ctx setPersistentStoreCoordinator: self.coord];
-    
-    /* Test Data */
     NSDictionary* s = [[NSDictionary alloc] initWithObjectsAndKeys:@"RECT", @"uuid", nil];
     rs = [NUResponseSet newResponseSetForSurvey:s withModel:self.model inContext:self.ctx];
     rs.uuid = @"OVAL";
@@ -40,29 +24,9 @@ NUResponseSet* rs;
     [rs newResponseForQuestion:@"xyz" Answer:@"456" Value:@"bar"];
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-	self.ctx = nil;
-	NSError *error = nil;
-	STAssertTrue([self.coord removePersistentStore: self.store error: &error], 
-                 @"couldn't remove persistent store: %@", error);
-	self.store = nil;
-	self.coord = nil;
-	self.model = nil;
-    self.bundle = nil;
-	
-    [super tearDown];
-}
-
-- (void)testThatEnvironmentWorks {
-	STAssertNotNil(self.store, @"no persistent store");
-}
-
 - (void)testSanity {
     NUResponseSet* rs = [NUResponseSet newResponseSetForSurvey:[NSDictionary dictionary] withModel:self.model inContext:self.ctx];
     [rs newResponseForIndexQuestion:@"abc" Answer:@"123"];
-    
     
     STAssertEquals(1U, [[rs responsesForQuestion:@"abc" Answer:@"123"] count], @"Should have one element");
 }
