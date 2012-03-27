2bd1abcb34b4a028056cc5bae90ba8827a13687b
John Dzak <john.dzak@gmail.com>
2012-03-27 14:36:42 -0500

    Create database test super class.

--- NUSurveyorTests/NUResponseTest.h
+++ NUSurveyorTests/NUResponseTest.h
@@ -6,18 +6,10 @@
 //  Copyright (c) 2012 Northwestern University. All rights reserved.
 //
 
-//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
-//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
-
 #import <SenTestingKit/SenTestingKit.h>
-#import <CoreData/CoreData.h>
 
-@interface NUResponseTest : SenTestCase
+#import "NUDatabaseTest.h"
 
-@property (nonatomic, strong) NSBundle *bundle;
-@property (nonatomic, strong) NSPersistentStoreCoordinator *coord;
-@property (nonatomic, strong) NSManagedObjectContext *ctx;
-@property (nonatomic, strong) NSManagedObjectModel *model;
-@property (nonatomic, strong) NSPersistentStore *store;
+@interface NUResponseTest : NUDatabaseTest
 
 @end
--- NUSurveyorTests/NUResponseTest.m
+++ NUSurveyorTests/NUResponseTest.m
@@ -12,58 +12,21 @@
 
 @implementation NUResponseTest
 
-@synthesize bundle = _bundle, coord = _coord, ctx = _ctx, model = _model, store = _store;
-
 NUResponse* response;
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-	
-    // Set-up code here.
-	self.bundle = [NSBundle bundleWithIdentifier:@"NUBIC.NUSurveyorTests"];
-	self.model = [[NSManagedObjectModel alloc] initWithContentsOfURL:[self.bundle URLForResource:@"NUSurveyor" withExtension:@"mom"]];
-	self.coord = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: self.model];
-	self.store = [self.coord addPersistentStoreWithType: NSInMemoryStoreType
-                                          configuration: nil
-                                                    URL: nil
-                                                options: nil 
-                                                  error: NULL];
-	self.ctx = [[NSManagedObjectContext alloc] init];
-	[self.ctx setPersistentStoreCoordinator: self.coord];
-    
-    /* Test Data */
+
     NSDictionary* s = [[NSDictionary alloc] initWithObjectsAndKeys:@"RECT", @"uuid", nil];
     NUResponseSet* rs = [NUResponseSet newResponseSetForSurvey:s withModel:self.model inContext:self.ctx];
     response = [rs newResponseForQuestion:@"abc" Answer:@"123" Value:@"foo"];
     [response setValue:@"OCT" forKey:@"uuid"];
 }
 
-
-- (void)tearDown
-{
-    // Tear-down code here.
-	self.ctx = nil;
-	NSError *error = nil;
-	STAssertTrue([self.coord removePersistentStore: self.store error: &error], 
-                 @"couldn't remove persistent store: %@", error);
-	self.store = nil;
-	self.coord = nil;
-	self.model = nil;
-    self.bundle = nil;
-	
-    [super tearDown];
-}
-
-- (void)testThatEnvironmentWorks {
-	STAssertNotNil(self.store, @"no persistent store");
-}
-
 - (void)testSanity {
     NUResponseSet* rs = [NUResponseSet newResponseSetForSurvey:[NSDictionary dictionary] withModel:self.model inContext:self.ctx];
     [rs newResponseForIndexQuestion:@"abc" Answer:@"123"];
-    
-    
+        
     STAssertEquals(1U, [[rs responsesForQuestion:@"abc" Answer:@"123"] count], @"Should have one element");
 }
 
