f1db45dbc885437c2d4a8a4fe53f56ad10d12d03
John Dzak <john.dzak@gmail.com>
2012-07-27 16:11:26 -0500

    Add an target for application tests. Task #24.

---
+++ NUSurveyorAppTests/NUSectionTVCTest.h
@@ -0,0 +1,16 @@
+//
+//  NUSectionTVCTest.h
+//  NUSurveyor
+//
+//  Created by John Dzak on 7/27/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+//  Application unit tests contain unit test code that must be injected into an application to run correctly.
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NUSectionTVCTest : SenTestCase
+
+@end
---
+++ NUSurveyorAppTests/NUSectionTVCTest.m
@@ -0,0 +1,42 @@
+//
+//  NUSectionTVCTest.m
+//  NUSurveyor
+//
+//  Created by John Dzak on 7/27/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import "NUSectionTVCTest.h"
+
+#import "NUSectionTVC.h"
+#import "JSONKit.h"
+#import "NUSurvey.h"
+
+@implementation NUSectionTVCTest
+
+// All code under test is in the iOS Application
+- (void)testAppDelegate
+{
+    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+}
+
+- (void)testNoRows {
+    NUSectionTVC* t = [NUSectionTVC new];
+    t.detailItem = nil;
+    STAssertNil(t.allSections, @"Should be nil");
+    STAssertNil(t.visibleSections, @"Should be nil");
+}
+
+- (void)testOneRow {
+    NUSectionTVC* t = [[NUSectionTVC alloc] initWithStyle:UITableViewStyleGrouped];
+    t.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 750, 1000)];
+    t.tableView = [UITableView new];
+    t.tableView.bounds = CGRectMake(0, 0, 200, 200);
+    NSString* q = @"{\"questions_and_groups\":[{\"text\":\"These questions are examples of the basic supported input types\",\"uuid\":\"764f9400-4540-012f-9ea4-00254bc472f4\",\"type\":\"label\"}]}";
+    t.detailItem = [q objectFromJSONStringWithParseOptions:JKParseOptionStrict];
+    STAssertEquals(1U, [t.allSections count], @"Should have one row");
+    STAssertEquals(1U, [t.visibleSections count], @"Should have one row");
+}
+
+@end
