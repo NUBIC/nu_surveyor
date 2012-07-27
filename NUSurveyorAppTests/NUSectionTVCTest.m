f7d73f89b8e2927743154ef3eeba06657908e662
John Dzak <john.dzak@gmail.com>
2012-07-27 17:25:51 -0500

    Added basic tests for row creation. Task #24.

--- NUSurveyorAppTests/NUSectionTVCTest.m
+++ NUSurveyorAppTests/NUSectionTVCTest.m
@@ -8,31 +8,44 @@
 
 #import "NUSectionTVCTest.h"
 
+#import "NUAppDelegate.h"
 #import "NUSectionTVC.h"
 #import "JSONKit.h"
 #import "NUSurvey.h"
 
 @implementation NUSectionTVCTest
 
-// All code under test is in the iOS Application
+static NUSectionTVC* t;
+
+- (void) setUp {
+    t = [[NUSectionTVC alloc] initWithStyle:UITableViewStyleGrouped];
+    NUAppDelegate* delegate = ((NUAppDelegate *)[[UIApplication sharedApplication] delegate]);
+    
+    NSEntityDescription *entity =
+    [[[delegate managedObjectModel] entitiesByName] objectForKey:@"ResponseSet"];
+    NUResponseSet *rs = [[NUResponseSet alloc]
+                         initWithEntity:entity insertIntoManagedObjectContext:[delegate managedObjectContext]];
+    
+    t.responseSet = rs;
+}
+
 - (void)testAppDelegate {
     id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
     STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
 }
 
 - (void)testNoRows {
-    NUSectionTVC* t = [NUSectionTVC new];
     t.detailItem = nil;
     STAssertNil(t.allSections, @"Should be nil");
     STAssertNil(t.visibleSections, @"Should be nil");
 }
 
 - (void)testOneRow {
-    NUSectionTVC* t = [[NUSectionTVC alloc] initWithStyle:UITableViewStyleGrouped];
     NSString* q = @"{\"questions_and_groups\":[{\"text\":\"These questions are examples of the basic supported input types\",\"uuid\":\"764f9400-4540-012f-9ea4-00254bc472f4\",\"type\":\"label\"}]}";
     t.detailItem = [q objectFromJSONStringWithParseOptions:JKParseOptionStrict];
     STAssertEquals(1U, [t.allSections count], @"Should have one row");
     STAssertEquals(1U, [t.visibleSections count], @"Should have one row");
 }
+
 
 @end
