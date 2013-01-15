f23da4af0eece1eb820a2ffce0dda7cd88fb188b
John Dzak <john.dzak@gmail.com>
2013-01-15 16:47:18 -0600

    Optimize. #101

--- NUSurveyor/Models/NSDateFormatter+NUAdditions.m
+++ NUSurveyor/Models/NSDateFormatter+NUAdditions.m
@@ -8,36 +8,49 @@
 
 #import "NSDateFormatter+NUAdditions.h"
 
+static NSDateFormatter* rfc3339DateFormatter;
+static NSDateFormatter* dateTimeResponseFormatter;
+static NSDateFormatter* dateResponseFormatter;
+static NSDateFormatter* timeResponseFormatter;
+
 @implementation NSDateFormatter (NUAdditions)
 
 + (NSDateFormatter*) rfc3339DateFormatter {
-    NSDateFormatter* f = [[NSDateFormatter alloc] init];
-    [f setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
-    [f setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZZZ"];
-    [f setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-    return f;
+    if (!rfc3339DateFormatter) {
+        rfc3339DateFormatter = [[NSDateFormatter alloc] init];
+        [rfc3339DateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+        [rfc3339DateFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZZZ"];
+        [rfc3339DateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    }
+    return rfc3339DateFormatter;
 }
 
 + (NSDateFormatter*) dateTimeResponseFormatter {
-    NSDateFormatter* f = [[NSDateFormatter alloc] init];
-    [f setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
-    [f setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mmZZZZZ"];
-    [f setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-    return f;
+    if (!dateTimeResponseFormatter) {
+        dateTimeResponseFormatter = [[NSDateFormatter alloc] init];
+        [dateTimeResponseFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+        [dateTimeResponseFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mmZZZZZ"];
+        [dateTimeResponseFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    }
+    return dateTimeResponseFormatter;
 }
 
 + (NSDateFormatter*) dateResponseFormatter {
-    NSDateFormatter* f = [[NSDateFormatter alloc] init];
-    [f setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
-    [f setDateFormat:@"yyyy-MM-dd"];
-    return f;
+    if (!dateResponseFormatter) {
+        dateResponseFormatter = [[NSDateFormatter alloc] init];
+        [dateResponseFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+        [dateResponseFormatter setDateFormat:@"yyyy-MM-dd"];
+    }
+    return dateResponseFormatter;
 }
 
 + (NSDateFormatter*) timeResponseFormatter {
-    NSDateFormatter* f = [[NSDateFormatter alloc] init];
-    [f setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
-    [f setDateFormat:@"HH:mm"];
-    return f;
+    if (!timeResponseFormatter) {
+        timeResponseFormatter = [[NSDateFormatter alloc] init];
+        [timeResponseFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+        [timeResponseFormatter setDateFormat:@"HH:mm"];
+    }
+    return timeResponseFormatter;
 }
 
 @end
